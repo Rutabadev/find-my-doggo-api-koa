be253a80b5356245ef7806773f27599784b9a155
Etienne RABY <etienne.raby@fittingbox.com>
2020-12-07 14:16:12 +0100

    display version on hello endpoint

--- src/controller/general.ts
+++ src/controller/general.ts
@@ -1,5 +1,6 @@
 import { BaseContext } from 'koa';
 import { description, request, summary, tagsAll } from 'koa-swagger-decorator';
+import packageJson from '../../package.json';
 
 @tagsAll(['General'])
 export default class GeneralController {
@@ -9,6 +10,6 @@ export default class GeneralController {
       'A simple welcome message to verify the service is up and running.'
    )
    public static async helloWorld(ctx: BaseContext): Promise<void> {
-      ctx.body = "I'm up all night to get doggy";
+      ctx.body = `I'm up all night to get doggy (v${packageJson.version})`;
    }
 }
